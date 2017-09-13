33bc14f527a3bfc62ae132b2cc09746f181ee36c
Aditya Kumar <aditya.podia@gmail.com>
2017-09-14 00:56:28 +0530

    feat(plugin): implemented the definition

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ npm install grunt-akp-devnotes --save-dev
 Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
 
 ```js
+grunt.loadNpmTasks('grunt-template');
 grunt.loadNpmTasks('grunt-akp-devnotes');
 ```
 
@@ -34,6 +35,22 @@ grunt.initConfig({
   },
 });
 ```
+### Package.json
+
+```js
+{
+  "name": "my-akp-devnotes",
+  "version": "0.0.0",
+  "dependencies": {},
+  "devDependencies": {
+    "grunt": "~0.4.2",
+    "grunt-akp-devnotes": "^0.1.1",
+    "grunt-template": "^0.2.3"
+  },
+  "engines": {
+    "node": ">=0.8.0"
+  }
+}
 
 ### Options
 
@@ -55,31 +72,37 @@ A string value that is used to do something else with whatever else.
 In this example, the default options are used to do something with whatever. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result would be `Testing, 1 2 3.`
 
 ```js
-grunt.initConfig({
-  akp_devnotes: {
-    options: {},
-    files: {
-      'dest/default_options': ['src/testing', 'src/123'],
-    },
-  },
-});
+    grunt.initConfig({
+      akp_devnotes: {
+          options: {
+            src: 'src/',
+            dest: 'dist/',
+            excludeComponents: ['devNotes', 'message', 'position', 'transition', 'utilities'],
+            partialsTemplate:'app/devnotes-template.html'
+          },
+          files: {
+            'dest/default_options': ['src/srcfile', 'src/123'],
+          }
+        },
+      });
 ```
 
 #### Custom Options
 In this example, custom options are used to do something else with whatever else. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result in this case would be `Testing: 1 2 3 !!!`
 
 ```js
-grunt.initConfig({
-  akp_devnotes: {
-    options: {
-      separator: ': ',
-      punctuation: ' !!!',
-    },
-    files: {
-      'dest/default_options': ['src/testing', 'src/123'],
-    },
-  },
-});
+    grunt.initConfig({
+      akp_devnotes: {
+          options: {
+            dest: 'destination/paterns-par',
+            excludeComponents: ['devNotes', 'message', 'position', 'transition', 'utilities'],
+            partialsTemplate:'app/devnotes-template.html'
+          },
+          files: {
+            'dest/default_options': ['src/srcfile', 'src/123'],
+          }
+        },
+      });
 ```
 
 ## Contributing
--- tasks/akp_devnotes.js
+++ tasks/akp_devnotes.js
@@ -12,155 +12,91 @@ module.exports = function(grunt) {
     // creation: http://gruntjs.com/creating-tasks
     grunt.registerMultiTask('akp_devnotes', 'picks js, html, readme and put them together to create devnotes', function() {
         // Merge task-specific and/or target-specific options with these defaults.
-        var pkg = grunt.file.readJSON('package.json');
-        var foundModules = {};
-
         var options = this.options({
-            args: [],
-            dest: 'dist',
-            allowedComponents: [],
-            libraryName: '',
-            concatDest: '',
-            concatTaskName: '',
-            nameofpartialfile:''
+            src: 'src/',
+            dest: 'dist/',
+            excludeComponents: ['devNotes', 'message', 'position', 'transition', 'utilities'],
+            partialsTemplate:'devnotes-template.html'
         });
+
+        console.log('\x1b[45m%s\x1b[0m', 'src:= ' + options.src);   
+        console.log('\x1b[45m%s\x1b[0m', 'dest:= ' + options.dest);
+        console.log('\x1b[45m%s\x1b[0m', 'excludeComponents:= ' + options.excludeComponents);   
+        console.log('\x1b[45m%s\x1b[0m', 'partialsTemplate:= ' + options.partialsTemplate);   
+        var lastChar = options.dest.substr(-1);
+        if (lastChar != '/') {
+            options.dest = options.dest + '/';
+        }
+        lastChar = options.src.substr(-1);
+        if (lastChar != '/') {
+            options.src  = options.src + '/';
+        }
+        console.log('\x1b[45m%s\x1b[0m', 'src:= ' + options.src);
+        console.log('\x1b[45m%s\x1b[0m', 'dest:= ' + options.dest);
+                
+        var isModuleProcessed = {};
+        
         function _contains(array, searchString) {
             return (array.indexOf(searchString) > -1);
         }
-        function findModule(name) {
-            if (foundModules[name]) { 
+
+        function processModule(name) {
+            if (isModuleProcessed[name]) { 
               return;
             }
-            foundModules[name] = true;
-            function breakup(text, separator) {
-                return text.replace(/[A-Z]/g, function (match) {
-                  return separator + match;
-                });
-            }
-            function ucwords(text) {
-                return text.replace(/^([a-z])|\s+([a-z])/g, function ($1) {
-                  return $1.toUpperCase();
-                });
-            }
-            function enquote(str) {
-              return '"' + str + '"';
-            }
+            isModuleProcessed[name] = true;
+
             var module = {
                 name: name,
-                moduleName: enquote('ddh.att.' + name),
-                //displayName: ucwords(breakup(name, ' ')),
                 displayName: name,
-                srcFiles: grunt.file.expand("src/" + name + "/*.js"),
-                ctrlFiles: grunt.file.expand("src/" + name + "/docs/demo.js"),
-                dependencies: dependenciesForModule(name),
                 docs: {
-                    js: grunt.file.expand("src/" + name + "/docs/demo.js")
+                    js: grunt.file.expand(options.src + name + "/docs/demo.js")
                     .map(grunt.file.read).join("\n"),
-                    html: grunt.file.expand("src/" + name + "/docs/demo.html")
+                    html: grunt.file.expand(options.src + name + "/docs/demo.html")
                     .map(grunt.file.read).join("\n"),
-                    devn: grunt.file.expand("src/" + name + "/docs/readme.md")
+                    devn: grunt.file.expand(options.src + name + "/docs/readme.md")
                     .map(grunt.file.read).join("\n"),
-                    history: grunt.file.expand("src/" + name + "/docs/history.md")
+                    history: grunt.file.expand(options.src + name + "/docs/history.md")
                     .map(grunt.file.read).join("\n")							
                 } 
             };
-            module.dependencies.forEach(findModule);
             grunt.config('modules', grunt.config('modules').concat(module));
-            var exclusion = ['devNotes', 'message', 'position', 'transition', 'utilities'];
+            var templateData = {
+                'name': name,
+                'html': module.docs.html,
+                'devn': module.docs.devn,
+                'history':module.docs.history,
+                'js': module.docs.js
+            };
+            name = name.replace(/([A-Z])/g, '-$1').toLowerCase();
+            var destsrc = options.dest + '/' + 'patterns-' + name + '.html';
+            var templateFiles = {};
 
-            if (exclusion.indexOf(name) == -1) {
-                var templateData = {
-                    'name': name,
-                    'html': module.docs.html,
-                    'devn': module.docs.devn,
-                    'history':module.docs.history,
-                    'js': module.docs.js
-                };
-                name = name.replace(/([A-Z])/g, '-$1').toLowerCase();
-                var destsrc = options.dest + '/' + 'patterns-' + name + '.html';
-                var templateFiles = {};
-
-                if(options.nameofpartialfile === undefined || options.nameofpartialfile === "" || options.nameofpartialfile === null){
-                    templateFiles[destsrc] = 'app/partials.html';
-                }
-                else{
-                    templateFiles[destsrc] = options.nameofpartialfile;
-                }
-                grunt.config('template.' + name + '.options', {
-                    'data': templateData
-                });
-                grunt.config('template.' + name + '.files', templateFiles);
-            }
-        }
-        function dependenciesForModule(name) {
-            var deps = [];
-            grunt.file.expand('src/' + name + '/*.js')
-                .map(grunt.file.read)
-                .forEach(function (contents) {
-                    //Strategy: find where module is declared,
-                    //and from there get everything inside the [] and split them by comma
-                    var moduleDeclIndex = contents.indexOf('angular.module(');
-                    var depArrayStart = contents.indexOf('[', moduleDeclIndex);
-                    var depArrayEnd = contents.indexOf(']', depArrayStart);
-                    var dependencies = contents.substring(depArrayStart + 1, depArrayEnd);
-                    dependencies.split(',').forEach(function (dep) {
-                        if (dep.indexOf('ddh.att.') > -1) {
-                            var depName = dep.trim().replace('ddh.att.', '').replace(/['"]/g, '');
-                            if (deps.indexOf(depName) < 0) {
-                                deps.push(depName);
-                                //Get dependencies for this new dependency
-                                deps = deps.concat(dependenciesForModule(depName));
-                            }
-                        }
-                    });
-                });
-            return deps;
+            templateFiles[destsrc] = options.partialsTemplate;
+            grunt.config('template.' + name + '.options', {
+                'data': templateData
+            });
+            grunt.config('template.' + name + '.files', templateFiles);
         }
 
         function createPartials() {
+            console.log('\x1b[34m%s\x1b[0m', "Begining createPartials()");
             var _ = grunt.util._;
             grunt.config('modules', []);
             grunt.config('template', {});
-            foundModules = {};
-
-            //If arguments define what modules to build, build those. Else, everything
-            if ((options.args) && (options.args.length)) {
-                options.args.forEach(findModule);
-            } 
-            else {
-                grunt.file.expand({
-                    filter: 'isDirectory',
-                    cwd: '.'
-                },
-                'src/*').forEach(function (dir) {
-                    if (_contains(options.allowedComponents, dir.split('/')[1])) {
-                        findModule(dir.split('/')[1]);
-                    }
-                });
-            }
-            var modules = grunt.config('modules');
-            var srcFiles = _.pluck(modules, 'srcFiles');
-            var controllers = _.pluck(modules, 'ctrlFiles');
-
-            var concatTaskName = options.concatTaskName;
-            var concatDest = options.concatDest;
-            if (options.libraryName) {
-                grunt.config('concat.dist.src', grunt.config('concat.dist.src').concat(srcFiles));
-                grunt.config('concat.dist.dest', options.libraryName);
-
-                if (concatTaskName && concatDest) {
-                grunt.config('concat.' + concatTaskName + '.src', grunt.config('concat.' + concatTaskName + '.src').concat(controllers));
-                grunt.config('concat.' + concatTaskName + '.dest', concatDest);
+            grunt.file.expand({
+                filter: 'isDirectory',
+                cwd: '.'
+            },
+            options.src + '*').forEach(function (dir) { //AK0192
+                if (!_contains(options.excludeComponents, dir.split('/')[1])) {
+                    processModule(dir.split('/')[1]);
                 }
-                grunt.task.run(['template']);
-            } 
-            else {
-                if (concatTaskName && concatDest) {
-                    grunt.config('concat.' + concatTaskName + '.src', grunt.config('concat.' + concatTaskName + '.src').concat(srcFiles));
-                    grunt.config('concat.' + concatTaskName + '.dest', concatDest);
-                }
-            }
+            });
+            grunt.task.run(['template']);
+            console.log('\x1b[34m%s\x1b[0m', "Ending createPartials()");
         }
+
         createPartials();
     });
 };
